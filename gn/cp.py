e5ecaab9ed82bb09f8f6a7a7d6f9526710d6b760
Stephen White <senorblanco@chromium.org>
2023-09-18 16:14:26 -0400

    Remove jversion.h.
    Generate it from jversion.h.in in libjpeg-turbo.

---
+++ gn/cp.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+# Copyright 2023 The Toucan Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import shutil
+import sys
+
+src, dst = sys.argv[1:]
+
+if os.path.exists(dst):
+  if os.path.isdir(dst):
+    shutil.rmtree(dst)
+  else:
+    os.remove(dst)
+
+if os.path.isdir(src):
+  shutil.copytree(src, dst)
+else:
+  shutil.copy2(src, dst)
