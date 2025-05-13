d701d39cf92309cda83d71f13dae5dc8454a3d96
Stephen White <senorblanco@chromium.org>
2025-05-13 11:17:50 -0400

    Search pipeline parent classes for shaders.
    During RenderPipeline creation, traverse the class hiearchy looking
    for vertex and fragment shaders. This allows a pipeline to inherit the
    vertex shader from its parent, for example.

--- api/api_dawn.cc
+++ api/api_dawn.cc
@@ -781,21 +781,21 @@ RenderPipeline* RenderPipeline_RenderPipeline(int               qualifiers,
   if (!type->IsClass()) { return nullptr; }
   ClassType*         classType = static_cast<ClassType*>(type);
   wgpu::ShaderModule vertexShader, fragmentShader;
-  for (auto& method : classType->GetMethods()) {
-    if (method->modifiers & Method::Modifier::Vertex) {
-      if (vertexShader) {
-        assert(!"more than one vertex shader specified");
-        return nullptr;
+  for (ClassType* c = classType; c != nullptr && (!vertexShader || !fragmentShader);
+       c = c->GetParent()) {
+    for (auto& method : c->GetMethods()) {
+      if (method->modifiers & Method::Modifier::Vertex) {
+        if (!vertexShader) {
+          vertexShader = createShaderModule(device, method.get());
+        }
+      } else if (method->modifiers & Method::Modifier::Fragment) {
+        if (!fragmentShader) {
+          fragmentShader = createShaderModule(device, method.get());
+        }
       }
-      vertexShader = createShaderModule(device, method.get());
-    } else if (method->modifiers & Method::Modifier::Fragment) {
-      if (fragmentShader) {
-        assert(!"more than one fragment shader specified");
-        return nullptr;
-      }
-      fragmentShader = createShaderModule(device, method.get());
     }
   }
+  assert(vertexShader && fragmentShader);
   PipelineLayout pipelineLayout;
   auto dawnBlendState = toDawnBlendState(*blendState);
   ExtractPipelineLayout(classType, device, &dawnBlendState, &pipelineLayout);
---
+++ test/interactive/triangle-with-inherited-vertex-shader.t
@@ -0,0 +1,25 @@
+using Vertex = float<4>;
+var device = new Device();
+var window = new Window({0, 0}, System.GetScreenSize());
+var swapChain = new SwapChain<PreferredSwapChainFormat>(device, window);
+var verts : [3]Vertex = { { 0.0, 1.0, 0.0, 1.0 }, {-1.0, -1.0, 0.0, 1.0 }, { 1.0, -1.0, 0.0, 1.0 } };
+var vb = new vertex Buffer<[]Vertex>(device, &verts);
+class PipelineBase {
+  vertex main(vb : &VertexBuiltins) { vb.position = vertices.Get(); }
+  var vertices : *VertexInput<Vertex>;
+}
+class Pipeline : PipelineBase {
+  fragment main(fb : &FragmentBuiltins) { fragColor.Set( {0.0, 1.0, 0.0, 1.0} ); }
+  var fragColor : *ColorAttachment<PreferredSwapChainFormat>;
+}
+var pipeline = new RenderPipeline<Pipeline>(device);
+var encoder = new CommandEncoder(device);
+var vi = new VertexInput<Vertex>(vb);
+var fb = swapChain.GetCurrentTexture().CreateColorAttachment(LoadOp.Clear);
+var renderPass = new RenderPass<Pipeline>(encoder, { vertices = vi, fragColor = fb });
+renderPass.SetPipeline(pipeline);
+renderPass.Draw(3, 1, 0, 0);
+renderPass.End();
+device.GetQueue().Submit(encoder.Finish());
+swapChain.Present();
+while (System.IsRunning()) System.GetNextEvent();
